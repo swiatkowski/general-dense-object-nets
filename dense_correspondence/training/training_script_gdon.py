3d0b0aebc2de2225f0629fd7a177cabde5e1a519
Jan Kopański <jan@kopanski.eu>
2020-03-28 00:21:51 +0100

    add evaluation to training script

--- dense_correspondence/training/training_script_gdon.py
+++ dense_correspondence/training/training_script_gdon.py
@@ -1,8 +1,13 @@
+# Prevent no $DISPLAY environment variable error from tkinter
+import matplotlib
+matplotlib.use('Agg')
+
 import dense_correspondence_manipulation.utils.utils as utils
 utils.add_dense_correspondence_to_python_path()
 from dense_correspondence.training.training import *
 import sys
 import logging
+import os
 
 #utils.set_default_cuda_visible_devices()
 utils.set_cuda_visible_devices([0]) # use this to manually set CUDA_VISIBLE_DEVICES
@@ -24,7 +29,7 @@ train_config = utils.getDictFromYamlFilename(train_config_file)
 dataset = SpartanDataset(config=config)
 
 logging_dir = "trained_models/tutorials"
-num_iterations = 3500
+num_iterations = 500
 d = 3 # the descriptor dimension
 name = "caterpillar_%d" %(d)
 train_config["training"]["logging_dir_name"] = name
@@ -35,7 +40,7 @@ train_config["training"]["num_iterations"] = num_iterations
 TRAIN = True
 EVALUATE = True
 
-
+# This statement is not true on Entropy
 # All of the saved data for this network will be located in the
 # code/data/pdc/trained_models/tutorials/caterpillar_3 folder
 
@@ -44,3 +49,13 @@ if TRAIN:
     train = DenseCorrespondenceTraining(dataset=dataset, config=train_config)
     train.run()
     print "finished training descriptor of dimension %d" %(d)
+
+
+code_dir = os.environ['HOME'] + '/code'
+model_folder = os.path.join(code_dir, logging_dir, name)
+model_folder = utils.convert_data_relative_path_to_absolute_path(model_folder)
+
+if EVALUATE:
+    DCE = DenseCorrespondenceEvaluation
+    num_image_pairs = 100
+    DCE.run_evaluation_on_network(model_folder, num_image_pairs=num_image_pairs)
