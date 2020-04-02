8e3d02785e20bdd8a9a0995476de107d46e0f53a
gasiortomasz <gasior.tt@gmail.com>
2020-04-02 23:34:39 +0200

    Add abstract and concrete logging integration

---
+++ dense_correspondence/logging/dispatcher.py
@@ -0,0 +1,13 @@
+from dense_correspondence.logging.NeptuneLogger import NeptuneLogger
+
+def dispatch_logger(config):
+    if config['logging']['backend'] == 'neptune':
+        return NeptuneLogger(config)
+    elif config['logging']['backend'] == 'tensorboard':
+        raise NotImplementedError('No support for "Tensorboard" yet')
+    elif config['logging']['backend'] == 'wandb':
+        raise NotImplementedError('No support for "Weights & Biases" yet')
+    elif config['logging']['backend'] == 'file':
+        raise NotImplementedError('No support for files yet')
+    else:
+        raise Exception("Logging backend: {} not recognized. Supported types are: [neptune]".format(type))
