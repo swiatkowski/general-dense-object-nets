c69b0c9d38e75f844f95bba66ed087d08be003ef
gasiortomasz <gasior.tt@gmail.com>
2020-04-15 12:58:40 +0200

    Update tag appending

--- dense_correspondence/logging/NeptuneLogger.py
+++ dense_correspondence/logging/NeptuneLogger.py
@@ -22,8 +22,11 @@ class NeptuneLogger(Logger):
     def append_tags_from_config(self, config):
         loss_function_config = config['loss_function']
         loss_name = loss_function_config['name']
-        sampler_name = loss_function_config['sampler']['name']
-        self.api.append_tag([loss_name, '{}-sampler'.format(sampler_name)])
+        self.api.append_tag([loss_name])
+
+        if loss_function_config['sampler']['name'] is not None:
+            sampler_name = loss_function_config['sampler']['name']
+            self.api.append_tag(['{}-sampler'.format(sampler_name)])
 
     def send_logs(self):
         for metric, value, type in self.storage:
