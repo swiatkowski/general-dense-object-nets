1cfca16c792c6199f1f02740ff867d09333f55bf
gasiortomasz <gasior.tt@gmail.com>
2020-04-23 14:02:50 +0200

    Update training scripts

--- job2.sh
+++ job2.sh
@@ -7,7 +7,7 @@
 #SBATCH -c 8
 /bin/hostname
 singularity exec --nv \
-  --bind /scidatasm/dense_object_nets/data:/home/$USER/data \
+  --bind /scidatalg/mlp2020_descriptors:/home/$USER/data \
   --bind /results/$USER/general-dense-object-nets:/home/$USER/code \
   /results/$USER/gdon_latest.sif \
   ~/code/dense_correspondence/training/train2.sh
