abcdea76b34d6f17448ae2ecfd914216b23b5099
Jan Kopański <jan@kopanski.eu>
2020-04-28 14:55:42 +0200

    PR fixes

--- dense_correspondence/training/training_script_gdon.py
+++ dense_correspondence/training/training_script_gdon.py
@@ -8,7 +8,7 @@ from dense_correspondence.training.training import *
 import sys
 import logging
 import os
-from random import randrange
+from time import strftime
 
 from dense_correspondence.training.training import DenseCorrespondenceTraining
 from dense_correspondence.dataset.spartan_dataset_masked import SpartanDataset
@@ -27,21 +27,10 @@ train_config = utils.getDictFromYamlFilename(train_config_file)
 data_config = utils.getDictFromYamlFilename(data_config_file)
 dataset = SpartanDataset(config=data_config)
 
-train_config['training']['logging_dir_name'] = '{0}_{1:8x}'.format(
-    train_config['logging']['experiment'].replace(' ', '_'), randrange(16**8))
+time_string = strftime('%d-%m-%Y_%X') # X=clock time ,%d day %m month %Y year
+train_config['training']['logging_dir_name'] = '{0}_{1}'.format(
+    train_config['logging']['experiment'].replace(' ', '_'), time_string)
 train_config['logging']['tags'].append(train_config['training']['logging_dir_name'])
 
-TRAIN = True
-EVALUATE = True
-
-if TRAIN:
-    train = DenseCorrespondenceTraining(dataset=dataset, config=train_config)
-    train.run()
-
-if EVALUATE:
-    model_folder = os.path.join(os.environ['HOME'], 'code',
-                                train_config['training']['logging_dir'],
-                                train_config['training']['logging_dir_name'])
-    num_image_pairs = 100
-    DenseCorrespondenceEvaluation.run_evaluation_on_network(model_folder,
-                                                            num_image_pairs=num_image_pairs)
+train = DenseCorrespondenceTraining(dataset=dataset, config=train_config)
+train.run()
